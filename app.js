6db8bbccd7439190f86b33efe58bf6d277344d38
Ajuogaaz <brianlinus1753@gmail.com>
2020-08-28 18:29:01 -0400

    challenge 1 and 2 done

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/" , (req, res)=>{
+  res.render("home", {firstBlog : homeStartingContent});
+});
 
 
 app.listen(port, function() {
